6469a57f1bc60e59d3c578089ccf1e7803b41b64
Marlon Porres <marlonporres75@gmail.com>
2026-09-07 19:36:31 -0600

    Estructura completada y main declarado

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+
+    print("==============================")
+    print(" SEMÁFOROS Y MONITORES")
+    print("==============================")
+    print("1. Demostración de semáforo")
+    print("2. Demostración de monitor")
+    print("0. Salir")
+
+
+if __name__ == "__main__":
+    main()
